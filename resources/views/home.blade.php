b46220c7d27e99c9f9ad3b9eb56f72d0aae78b1a
abhinavmathur96 <abhinavmathur96@gmail.com>
2015-12-19 14:30:52 +0530

    styling update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,8 +41,6 @@
             <form>
                 <div class="input-field">
                     <input id="search" type="search" placeholder="Enter Roll Number" required>
-                    <label for="search"><i class="material-icons">search</i></label>
-                    <i class="material-icons">close</i>
                 </div>
             </form>
         </div>
