a80802e52d0a872cc4c38c3d9b5ad65d410707b9
abhinavmathur96 <abhinavmathur96@gmail.com>
2015-12-24 23:55:23 +0530

    admin dashboard components

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -12,6 +12,7 @@
     <title>Admin Dashboard</title>
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="css/materialize.min.css">
+    <link rel="stylesheet" href="css/custom.css">
     
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
@@ -19,86 +20,128 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
-    <style>
-        #search
-        {
-            display: none;
-            margin-bottom: 0px;
-        }
-        
-        .input-field input[type=text]:focus {
-            border-bottom: 1px solid #000;
-            box-shadow: 0 1px 0 0 #000;
-        }
-        
-        i.full {
-            line-height: 2 !important;
-        }
-        
-        #slide-out {
-            margin-top: 65px;
-        }
-        
-        @media only screen and (max-width : 992px) {
-            #slide-out {
-                margin-top: 0px;
-            }
-        }
-    </style>
 
 </head>
     
 <body>
-    <nav>
-        <div class="nav-wrapper orange">
-            <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-            <a href="#" class="brand-logo"><i class="material-icons left full">fingerprint</i> BIOMETRICS</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a id="toggle_search" href="#"><i class="material-icons">file_upload</i></a></li>
-                <li><a href="#" class="dropdown-button" data-activates="dropdown1"><i class="material-icons">account_circle</i></a></li>
-            </ul>
-        </div>
-        <ul id="slide-out" class="side-nav full fixed">
-            <li><a href="#">Home</a></li>
-            <li class="no-padding">
-                <ul class="collapsible collapsible-accordion">
-                    <li>
-                        <a class="collapsible-header"><i class="material-icons right">arrow_drop_down</i>Students</a>
-                        <div class="collapsible-body">
-                            <ul>
-                                <li><a href="#">ADD</a></li>
-                                <li><a href="#">EDIT</a></li>
-                                <li><a href="#">DELETE</a></li>
-                            </ul>
-                        </div>
-                    </li>
+    <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper orange">
+                <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+                <a href="#" class="brand-logo"><i class="material-icons left full">fingerprint</i> BIOMETRICS</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a id="toggle_search" href="#"><i class="material-icons">file_upload</i></a></li>
+                    <li><a href="#" class="dropdown-button" data-activates="dropdown1"><i class="material-icons">account_circle</i></a></li>
                 </ul>
-            </li>
-            <li><a href="#">Courses</a></li>
-            <li><a href="#">Teachers</a></li>
-            <li><a href="#">View Information</a></li>
-        </ul>
-    </nav>
+            </div>
+            <ul id="slide-out" class="side-nav full fixed">
+                <li><a href="#">Home</a></li>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                        <li>
+                            <a class="collapsible-header"><i class="material-icons right">arrow_drop_down</i>Students</a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <li><a href="#">ADD</a></li>
+                                    <li><a href="#">EDIT</a></li>
+                                    <li><a href="#">DELETE</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                        <li>
+                            <a class="collapsible-header"><i class="material-icons right">arrow_drop_down</i>Courses</a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <li><a href="#">ADD</a></li>
+                                    <li><a href="#">EDIT</a></li>
+                                    <li><a href="#">DELETE</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                        <li>
+                            <a class="collapsible-header"><i class="material-icons right">arrow_drop_down</i>Teachers</a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <li><a href="#">ADD</a></li>
+                                    <li><a href="#">EDIT</a></li>
+                                    <li><a href="#">DELETE</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                        <li>
+                            <a class="collapsible-header"><i class="material-icons right">arrow_drop_down</i>View Information</a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <li><a href="#">Students</a></li>
+                                    <li><a href="#">Teachers</a></li>
+                                    <li><a href="#">Courses</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <div class="row">
+                    <div class="col s4">
+                        <a class="btn-flat waves-effect waves-red"><i class="material-icons">bug_report</i></a>
+                    </div>
+                    <div class="col s4">
+                        <a class="btn-flat waves-effect waves-orange"><i class="material-icons">contact_mail</i></a>
+                    </div>
+                </div>
+            </ul>
+        </nav>
+    </div>
     
     <ul id="dropdown1" class="dropdown-content">
         <li><a href="#">Settings</a></li>
         <li><a href="#">Logout</a></li>
     </ul>
     
-    <div id="search" class="row">
-        <div class="container">
-            <form action="#">
-                <div class="file-field input-field">
-                    <div class="btn white black-text waves-effect waves-orange">
-                        <span>File</span>
-                        <input type="file">
+    <!-- Start of padded div wrapper -->
+    <div id="padd">
+        <div id="search" class="row">
+            <div class="container">
+                <form action="#">
+                    <div class="file-field input-field">
+                        <div class="btn white black-text waves-effect waves-orange">
+                            <span>File</span>
+                            <input type="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text">
+                        </div>
                     </div>
-                    <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text">
-                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card-panel white hoverable">
+                    <p class="flow-text center-align">Students Registered<br>4903</p>
                 </div>
-            </form>
+            </div>
+            <div class="col s12 m4">
+                <div class="card-panel white hoverable">
+                    <p class="flow-text center-align">Teachers Registered<br>245</p>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card-panel white hoverable">
+                    <p class="flow-text center-align">Courses Running<br>   85</p>
+                </div>
+            </div>
         </div>
     </div>
     
